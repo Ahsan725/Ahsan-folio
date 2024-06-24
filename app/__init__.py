231ee0e69fd38a5041a4a58cb049fc664c19235b
Ahsan <sbaseer25@gmail.com>
2024-06-23 21:43:32 -0400

    Add about yourself section

--- app/__init__.py
+++ app/__init__.py
@@ -16,3 +16,7 @@ env = Environment(
 @app.route('/')
 def index():
     return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title="About Us")
