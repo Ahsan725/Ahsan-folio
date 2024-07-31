d3e7e579d51adc0225f94675abd483f02086f763
Ahsan <sbaseer25@gmail.com>
2024-07-31 14:58:53 -0400

    fixed typo

--- tests/test_db.py
+++ tests/test_db.py
@@ -11,7 +11,7 @@ class TestTimeLinePost(unittest.TestCase):
     def setUp(self):
         test_db.bind(MODELS, bind_refs=False, bind_backrefs=False)
         test_db.connect()
-        test_db.create_tabels(MODELS)
+        test_db.create_tables(MODELS)
     
     def tearDown(self):
         test_db.drop_tables(MODELS)
